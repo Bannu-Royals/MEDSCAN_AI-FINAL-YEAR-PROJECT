da0ace6ee8f03423a034c6d3496bdec24062ee0a
Durga Prasad Kakileti <durgaprasadkakileti@gmail.com>
2025-04-08 15:13:13 +0530

    app updated

--- app.py
+++ app.py
@@ -264,6 +264,6 @@ def tumor():
                         error=error)
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
 
